b19543cee5f5c20a60baad7c8e80ea0bde900619
arter97 <qkrwngud825@gmail.com>
2021-01-23 18:34:18 +0100

    X00TD: init: workaround SafetyNet check

--- init/init_X00TD.cpp
+++ init/init_X00TD.cpp
@@ -166,8 +166,49 @@ void vendor_check_variant()
     property_set("ro.config.versatility", region);
 }
 
+/* From Magisk@jni/magiskhide/hide_utils.c */
+static const char *snet_prop_key[] = {
+  "ro.boot.vbmeta.device_state",
+  "ro.boot.verifiedbootstate",
+  "ro.boot.flash.locked",
+  "ro.boot.selinux",
+  "ro.boot.veritymode",
+  "ro.boot.warranty_bit",
+  "ro.warranty_bit",
+  "ro.debuggable",
+  "ro.secure",
+  "ro.build.type",
+  "ro.build.tags",
+  "ro.build.selinux",
+  NULL
+};
+
+static const char *snet_prop_value[] = {
+  "locked",
+  "green",
+  "1",
+  "enforcing",
+  "enforcing",
+  "0",
+  "0",
+  "0",
+  "1",
+  "user",
+  "release-keys",
+  "1",
+  NULL
+};
+
+static void workaround_snet_properties() {
+    // Hide all sensitive props
+    for (int i = 0; snet_prop_key[i]; ++i) {
+        property_override(snet_prop_key[i], snet_prop_value[i]);
+    }
+}
+
 void vendor_load_properties()
 {
     init_alarm_boot_properties();
     vendor_check_variant();
+    workaround_snet_properties();
 }
